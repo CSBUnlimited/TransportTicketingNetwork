49805090f9801f0c2eddb5ef70033761d72134fa
lakshan chathura <lak.deve94@gmail.com>
2018-09-19 17:45:52 +0530

    Added route,subroute,busstop to the model

---
+++ Modules.Main.Models/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    class Route
+    {
+        public String RouteID { get; set; }
+
+        public String StartBusStopID { get; set; }
+
+        public int Distance { get; set; }
+
+    }
+}
---
+++ Modules.Main.Models/SubRoute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    class SubRoute
+    {
+        public String SubRouteId { get; set; }
+
+        public String RouteId { get; set; }
+
+        public String EndBusStopId { get; set; }
+
+        public int Distance { get; set; }
+
+        public int Fare { get; set; }
+    }
+}
