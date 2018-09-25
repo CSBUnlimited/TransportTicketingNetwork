3d8df2feb1db7d9948161755c01f4d403e2e7cbd
DeveloperLK <kaweelenaduwa@gmail.com>
2018-09-25 09:20:12 +0530

    Implemented Bus and Bus Schedule model classes

---
+++ Modules.Main.Models/Bus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    public class Bus
+    {
+        public int Id { get; set; }
+
+        public string BusName { get; set; }
+
+        public string BusNumber{ get; set; }
+
+        public string BusType { get; set; }
+
+        public int NoSeats { get; set; }
+
+        public string Description { get; set; }
+
+
+
+    }
+
+
+}
---
+++ Modules.Main.Models/BusSchedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    public class BusSchedule
+    {
+        public int Id { get; set; }
+        public string StartingPoint { get; set; }
+        public string Destination { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public string Duration { get; set; }
+
+        public BusStop StartBusStop { get; set; }
+        public BusStop EndBusStop { get; set; }
+
+        public ICollection<Bus>  AllocatedBus { get; set; }
+
+    }
+}
