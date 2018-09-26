c06374c57a6eca49cb791928fcdaac899a80a74e
lakshan chathura <lak.deve94@gmail.com>
2018-09-26 14:46:53 +0530

    Add Fare Model Class

---
+++ Modules.Main.Models/Fare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    public class Fare
+    {
+        public int Id { get; set; }
+
+        public int FareAmount { get; set; }
+
+        public int RouteId { get; set; }
+
+
+    }
+}
