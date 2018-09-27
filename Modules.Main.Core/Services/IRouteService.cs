611799e65597d7b2e61fed874269c882ec67d1f9
lakshan chathura <lak.deve94@gmail.com>
2018-09-27 11:34:03 +0530

    Updated Route Controller

--- Modules.Main.Core/Services/IRouteService.cs
+++ Modules.Main.Core/Services/IRouteService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Modules.Main.Core.Services
 {
-    public interface IRouteService :IService
+    public interface IRouteService : IService
     {
         //Method For Adding a Route
         Task<RouteViewModel> AddRoute(RouteViewModel routes);
