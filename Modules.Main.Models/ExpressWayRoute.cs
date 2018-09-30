6d5705fb8f6984acd22d8b7b1ac45c1da1b482c3
RavinduDharmasena <ravinduta@gmail.com>
2018-10-01 01:03:38 +0530

    Added New 3 Route Sub Model Classes.

---
+++ Modules.Main.Models/ExpressWayRoute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Main.Models
+{
+    public class ExpressWayRoute: Route
+    {
+        public string StartLocation { get; set; }
+
+        public string EndLocation { get; set; }
+
+        public float Distance { get; set; }
+
+    }
+}
