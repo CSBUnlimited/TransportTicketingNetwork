d3c17d0d20711cfbc2cd5f5ab95e9cc461f3997c
DeveloperLK <kaweelenaduwa@gmail.com>
2018-10-05 19:34:19 +0530

    Bus and BusSchedule Table Configurations added \

---
+++ TransportTicketingNetwork.Database/TableConfigurations/BSM/BusScheduleTableConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Modules.Main.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTicketingNetwork.Database.TableConfigurations.BSM
+{
+    public class BusScheduleTableConfiguration : IEntityTypeConfiguration<BusSchedule>
+    {
+        public void Configure(EntityTypeBuilder<BusSchedule> builder)
+        {
+            builder.ToTable("BusSchedules", "bssm");
+
+            //Id Mapping with auto increment
+
+            builder.Property(b => b.StartingPoint)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(b => b.Destination)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            //Start Time, End Time,Total Duration
+
+            //builder.HasOne(u => u.ApplicationUser).WithOne(au => au.User).HasForeignKey<User>(u => u.ApplicationUserId);
+
+        }
+    }
+}
---
+++ TransportTicketingNetwork.Database/TableConfigurations/BSM/BusTableConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Modules.Main.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTicketingNetwork.Database.TableConfigurations.BSM
+{
+    public class BusTableConfiguration : IEntityTypeConfiguration<Bus>
+    {
+        public void Configure(EntityTypeBuilder<Bus> builder)
+        {
+            builder.ToTable("Buses", "bsm");
+
+            //Id Mapping with auto increment
+
+            builder.Property(b => b.BusName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(b => b.BusNumber)
+                .HasMaxLength(5)
+                .IsRequired();
+
+            builder.Property(b => b.BusType)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(b => b.NoSeats)
+                .HasMaxLength(2)
+                .IsRequired();
+
+            builder.Property(b => b.Description)
+                .HasMaxLength(250)
+                .IsRequired();
+
+
+
+            //builder.HasOne(u => u.ApplicationUser).WithOne(au => au.User).HasForeignKey<User>(u => u.ApplicationUserId);
+
+        }
+    }
+}
